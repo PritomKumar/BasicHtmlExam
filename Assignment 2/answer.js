fbd0d5b44ef26ba7b60e5d3ecf47873a6a9c4041
PritomKumar <bsse0919@iit.du.ac.bd>
2020-07-04 13:39:31 +0600

    Add answer for question no 1 and 2 from class test 2

---
+++ Assignment 2/answer.js
@@ -0,0 +1,63 @@
+
+console.log("Answer to the Ques. No 1 ----");
+let array = [4,5,1,2,6,8,9,7,3];
+
+let largest = 0;
+
+for (let i=0 ; i< array.length ; i++){
+    if(largest<array[i]){
+        largest = array[i];
+    }
+}
+let secondLargest = 0;
+
+for (let i=0 ; i< array.length ; i++){
+    if(secondLargest<array[i] && largest!= array[i] ) {
+        secondLargest = array[i];
+    }
+}
+
+console.log( "Second largest = " + secondLargest );
+
+let lowest = 999999;
+
+for (let i=0 ; i< array.length ; i++){
+    if(lowest>=array[i]){
+        lowest = array[i];
+    }
+}
+let secondLowest = 99999;
+
+for (let i=0 ; i< array.length ; i++){
+    if(secondLowest>array[i] && lowest!= array[i] ) {
+        secondLowest = array[i];
+    }
+}
+
+console.log("Answer to the Ques. No 2 ----");
+
+console.log("Second answer ----");
+let array1 = [4,5,1,2,6,8,9,7,3,24,24,17,19,19,24];
+let visited = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+let countnumber=0;
+
+for(let i=0 ;i<array1.length ; i++){
+    let num = array1[i];
+    if(visited[i]== 1){
+        continue;
+    }
+    for(let j=0 ;j<array1.length ; j++){
+        if(array1[i]==array1[j] && visited[j]!= 1){
+            
+            countnumber++;
+            visited[j] = 1;
+            
+        }
+    }
+    console.log(  " Number = " + num + " count = " + countnumber);
+    countnumber=0;
+}
+
+
+
